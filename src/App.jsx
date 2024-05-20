d587e66c19675fabd973c731a235f706a60a0cb4
Marcin <maratajczak@gmail.com>
2024-05-20 15:44:15 +0200

    setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
+import { Button } from "@material-tailwind/react";
+import React from "react";
 
-import "./App.css";
-
-function App() {
+function Home() {
   return (
-    <>
-      <p>My blog</p>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click</Button>
+    </div>
   );
 }
 
-export default App;
+export default Home;
